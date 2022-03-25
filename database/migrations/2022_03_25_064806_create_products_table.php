b4a0cd3c0cf89abaae1f86f51330584ee92b7af0
sabalsaarturo <sabalsa29@gmail.com>
2022-03-25 01:00:50 -0600

    add_tables_productos_and_categories

---
+++ database/migrations/2022_03_25_064806_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku')->nullable();
+            $table->text('nombre')->nullable();
+            $table->integer('categoria_id')->default(0);
+            $table->longText('descripcion')->nullable();
+            $table->decimal('precio',7,2)->default(0);
+            $table->bigInteger('cantidad')->default(0);
+            $table->integer('estado')->default(0);
+            $table->integer('estatus')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
