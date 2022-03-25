e1af66a1a694eee5502b694f18a37485990cb5fa
sabalsaarturo <sabalsa29@gmail.com>
2022-03-25 13:00:11 -0600

    add_validations_form_create_product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,26 +8,28 @@
                 {!! Form::open(['url'=>'product/store', 'class'=>' row g-3']) !!}
                 <div class="col-md-6">
                   <label for="" class="form-label">Producto</label>
-                  <input name="nombre" type="text" class="form-control" placeholder="Nombre del producto">
+                  {{ Form::text('nombre',null,['class'=>'form-control','required','placeholder'=>'Nombre del Producto']) }}
                 </div>
                 <div class="col-md-6">
                     <label for="inputState" class="form-label">Categoria</label>
-                    {!! Form::select('categoria_id', $categorias, null, ['class'=>'form-select']) !!}
+                    {!! Form::select('categoria_id', $categorias, null, ['class'=>'form-select','require']) !!}
                 </div>
                 <div class="col-12">
                   <label for="" class="form-label">Descripción</label>
-                  <input name="descripcion" type="text" class="form-control" id="" placeholder="Descripcion del producto">
+                  {{ Form::text('descripcion',null,['class'=>'form-control','required','placeholder'=>'Descripcion del producto']) }}
+
                 </div>
                 <div class="col-md-6">
                   <label class="form-label">Cantidad</label>
-                  <input name="cantidad" type="number" class="form-control"  min="1">
+                  <input name="cantidad" require type="number" class="form-control" value="1" min="1">
                 </div>
                 <div class="col-md-6">
                     <label class="form-label">Precio</label>
-                    <input name="precio" type="number" class="form-control" min="0" step=".01">
+                    <input name="precio" type="number" class="form-control" require value="0" min="0" step=".01">
                   </div>
                 <div class="col-12">
-                  <button type="submit" class="btn btn-primary">Guardar</button>
+                    <a href="{{ url('/product') }}" class="btn btn-secondary"> Regresar</a>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
                 </div>
               {!! Form::close() !!}
         </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,6 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-12" style="background-color: #F8F9F9;">
             <h4>Listado de todos los productos</h4>
+            <div>
+                <a href="{{ url('product/create') }}" class="btn btn-primary btn-sm"> Nuevo Producto</a>
+                <br>
+                <hr>
+            </div>
+
                 @foreach ($productos as $item)
                 <div class="card mb-4 border-0" style="max-width: 750px;">
                     <div class="row g-0">
@@ -15,19 +21,22 @@
                             <div class="card-body">
                                 <h5 class="card-title">{{ $item->nombre }}</h5>
                                 <p class="card-text"></p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                <p class="card-text"><small class="text-primary">{{ $item->descripcion }}</small></p>
                             </div>
                         </div>
                         <div class="col-md-3" style=" border-left: 1px solid #DFDFDF">
                             <div class="card-body">
                                 <p class="text-primary text-center"><strong> {{ $item->precio }}</strong></p>
+
+                                <p class="text-sm-center text-success">Disponible</p>
+
                                 <p></p>
                                 <div class="d-grid gap-2 mx-auto">
                                     {!! Form::open(['url'=>'product/eliminar']) !!}
                                     {!! Form::hidden('id', $item->id) !!}
-                                    <button class="btn btn-light" style="width: 100%" type="submit">Eliminar</button>
+                                    <button class="btn btn-light btn-sm" style="width: 100%" type="submit">Eliminar</button>
                                     {!! Form::close() !!}
-                                    <button class="btn btn-primary" type="button">Calificar</button>
+                                    <button class="btn btn-primary btn-sm" type="button">Calificar</button>
                                   </div>
                             </div>
                         </div>
