70b4ff21a4ba76081c9ee87f1b8da53def3a1d0d
sabalsaarturo <sabalsa29@gmail.com>
2022-03-25 09:56:48 -0600

    add_relation_hasmany

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+
+    public function relCategory(){
+        return $this->hasMany(Product::class,'categoria_id','id');
+    }
 }
